7f675a3022bc6273a61527fb19512665cbbe71f3
Yaser Alnajjar <10493809+yaseralnajjar@users.noreply.github.com>
2026-05-13 01:48:33 +0200

    Route default installer endpoint

---
+++ apps/sifr-site/src/worker.js
@@ -0,0 +1,12 @@
+export default {
+  fetch(request, env) {
+    const url = new URL(request.url);
+
+    if (url.pathname === '/install') {
+      url.pathname = '/install/beta';
+      return env.ASSETS.fetch(new Request(url, request));
+    }
+
+    return env.ASSETS.fetch(request);
+  },
+};
